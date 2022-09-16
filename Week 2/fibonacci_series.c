c031fb41902f08f8547559101bb129f08b67e1cd
satiriorn <45328793+satiriorn@users.noreply.github.com>
2022-09-16 10:05:11 +0300

    Week 1 && week 2

---
+++ Week 2/fibonacci_series.c
@@ -0,0 +1,28 @@
+ /**
+  * @file fibonacci_series.c
+  * @brief The implementation file of second week of The Arduino Platform and C Programming
+  *
+  * This file includes implementations fibonacci series to 6 size
+  *
+  * @author Denys Astapieiev
+  * @date 22.08.2022
+  *
+  */
+
+#include <stdio.h>
+
+int main()
+{
+
+    int x = 0, y = 1, f_num = 0;
+    printf("Fibonacci series: ");
+    printf("%d %d ", x, y);
+
+    for (int i = 0; i < 4; i++)
+    {
+        f_num = x + y;
+        x = y;
+        y = f_num;
+        printf("%d ", f_num);
+    }
+}
